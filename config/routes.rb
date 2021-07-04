29771b7ef6b5659ed81e6407ec4002fd2d73fd64
zakinao <zakinaororo@gmail.com>
2021-07-04 11:35:59 +0900

    ticketモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
   root "welcome#index"
   get "auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  resources :events do
+    resources :tickets
+  end
 end
